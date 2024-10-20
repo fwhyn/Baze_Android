34d1a3b2321e39444974bfaee4b564cd3314de20
fwhyn <fwhynseventh@gmail.com>
2024-10-20 21:01:57 +0700

    Library Release
    added release properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+rootProject.run {
+    extra["lSdk"] = 24
+    extra["mSdk"] = 34
+
+    extra["javaVersion"] = JavaVersion.VERSION_1_8
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
