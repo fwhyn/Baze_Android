5c2b89cc8b1afc429dbbb057bd86008853101b1f
fwhyn <fwhynseventh@gmail.com>
2024-10-27 21:34:16 +0700

    Sample App
    edited configuration

--- appsample/build.gradle.kts
+++ appsample/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
 }
