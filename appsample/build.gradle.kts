1993e2657d145462e2618435f205002e30d93d6e
fwhyn <fwhynseventh@gmail.com>
2024-11-04 20:14:55 +0700

    Project Configuration
    edited gradle dependencies

--- appsample/build.gradle.kts
+++ appsample/build.gradle.kts
@@ -63,11 +63,11 @@ android {
 dependencies {
     // ----------------------------------------------------------------
     // Main Dependency
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
 
     // ----------------------------------------------------------------
     // Test Dependency
