740e44992012b0e48fbd8e97dc9770d2799ff55c
fwhyn <fwhynseventh@gmail.com>
2024-11-04 21:07:16 +0700

    Project Configuration
    edited gradle dependency

--- appsample/build.gradle.kts
+++ appsample/build.gradle.kts
@@ -65,6 +65,8 @@ android {
 dependencies {
     // ----------------------------------------------------------------
     // Main Dependency
+    implementation(project(mapOf("path" to ":baze")))
+
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
