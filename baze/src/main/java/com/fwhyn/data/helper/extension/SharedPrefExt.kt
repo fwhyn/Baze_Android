a79deab45916c833e8d8014de9fd60587dae1d4c
Yana Wahyuna <yana.wahyuna@epson.co.id>
2024-12-12 16:04:18 +0700

    Example App
    added implementation

--- baze/src/main/java/com/fwhyn/data/helper/extension/SharedPrefExt.kt
+++ baze/src/main/java/com/fwhyn/data/helper/extension/SharedPrefExt.kt
@@ -2,12 +2,11 @@ package com.fwhyn.data.helper.extension
 
 import android.content.SharedPreferences
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 
 inline fun <reified T> SharedPreferences.get(key: String): T? {
     val value = this.getString(key, null)
 
-    return GsonBuilder().create().fromJson(value, T::class.java)
+    return Gson().fromJson(value, T::class.java)
 }
 
 fun <T> SharedPreferences.put(key: String, data: T) {
