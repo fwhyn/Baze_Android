3f0de6fa571f9c12f1f0ccf6a1e798acc6901f96
Yana Wahyuna <yana.wahyuna@epson.co.id>
2024-12-13 17:05:26 +0700

    Example App
    updated dependency

--- appsample/src/main/java/com/fwhyn/appsample/di/CommonModule.kt
+++ appsample/src/main/java/com/fwhyn/appsample/di/CommonModule.kt
@@ -5,11 +5,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 
 @InstallIn(ActivityRetainedComponent::class)
 @Module
 class CommonModule {
 
     @Provides
+    @ActivityRetainedScoped
     fun provideMainUiState(): MainUiState = MainUiState()
 }
