8cc49051469b6ad2b1742543701231a7ce20935c
Yana Wahyuna <yana.wahyuna@epson.co.id>
2024-12-19 09:23:57 +0700

    SDK Up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 rootProject.run {
     extra["lSdk"] = 24
-    extra["mSdk"] = 34
+    extra["mSdk"] = 35
 
     extra["javaVersion"] = JavaVersion.VERSION_1_8
 
