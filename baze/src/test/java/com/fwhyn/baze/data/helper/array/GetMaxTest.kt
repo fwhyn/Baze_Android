52a4a26aa28a0766a34155bb7512ee874038971d
fwhyn <fwhynseventh@gmail.com>
2025-02-25 15:25:27 +0700

    Test
    added get min & get max test

---
+++ baze/src/test/java/com/fwhyn/baze/data/helper/array/GetMaxTest.kt
@@ -0,0 +1,30 @@
+package com.fwhyn.baze.data.helper.array
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GetMaxTest {
+
+    @Test
+    fun getMinTest() {
+        val input = arrayListOf(1, 2, 3, 5, 6)
+
+        assertEquals(6, getMax(input))
+
+        // ----------------------------------------------------------------
+        val input1 = arrayListOf(1, 2, 8, -5, 6)
+        getMax(input)
+
+        assertEquals(8, getMax(input1))
+    }
+
+    private fun getMax(input: List<Int>): Int {
+        var max = input[0]
+        for (i in 1..<input.size) {
+            val value = input[i]
+            if (value > max) max = value
+        }
+
+        return max
+    }
+}
---
+++ baze/src/test/java/com/fwhyn/baze/data/helper/array/GetMinTest.kt
@@ -0,0 +1,30 @@
+package com.fwhyn.baze.data.helper.array
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GetMinTest {
+
+    @Test
+    fun getMinTest() {
+        val input = arrayListOf(1, 2, 3, 5, 6)
+
+        assertEquals(1, getMin(input))
+
+        // ----------------------------------------------------------------
+        val input1 = arrayListOf(1, 2, 3, -5, 6)
+        getMin(input)
+
+        assertEquals(-5, getMin(input1))
+    }
+
+    private fun getMin(input: List<Int>): Int {
+        var min = input[0]
+        for (i in 1..<input.size) {
+            val value = input[i]
+            if (value < min) min = value
+        }
+
+        return min
+    }
+}
