58b8ea4c2b42e9c282d4fcd5de4304f5d6824982
fwhyn <fwhynseventh@gmail.com>
2025-02-25 12:46:36 +0700

    Util
    added @JvmStatic notation so can be accessed by Java file directly as static member

--- baze/src/main/java/com/fwhyn/baze/data/helper/Util.kt
+++ baze/src/main/java/com/fwhyn/baze/data/helper/Util.kt
@@ -9,11 +9,13 @@ import java.util.UUID
 
 object Util {
 
+    @JvmStatic
     fun getUniqueId(): String {
         val uuid = UUID.randomUUID().toString()
         return uuid.filterNot { it == '-' }
     }
 
+    @JvmStatic
     fun getDateTime(): String {
         // Get the current date and time
         val currentDateTime = Calendar.getInstance().time
@@ -25,6 +27,7 @@ object Util {
         return dateFormat.format(currentDateTime)
     }
 
+    @JvmStatic
     fun throwExceptionIfOutOfRange(value: Double, range: ClosedRange<Double>) {
         require(
             value >= range.start && value <= range.endInclusive,
@@ -32,18 +35,22 @@ object Util {
         )
     }
 
+    @JvmStatic
     fun throwTestMustNotFailed() {
         throw Exception("Test must not failed")
     }
 
+    @JvmStatic
     fun throwMustNoLoading() {
         throw Exception("Test must no loading")
     }
 
+    @JvmStatic
     fun throwMustNotSuccess() {
         throw Exception("Test must not success")
     }
 
+    @JvmStatic
     fun require(isOk: Boolean, exception: Exzeption = Exzeption()) {
         if (!isOk) throw exception
     }
