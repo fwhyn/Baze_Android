4e055919a262406062bdabe995269352a10f7c0f
fwhyn <fwhynseventh@gmail.com>
2025-03-01 15:54:55 +0700

    Test
    added sorted test

--- baze/src/test/java/com/fwhyn/baze/data/helper/array/SortedArrayTest.kt
+++ baze/src/test/java/com/fwhyn/baze/data/helper/array/SortedArrayTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class SortedArrayTest {
 
     @Test
-    fun bubbleSortedTest() {
+    fun bubbleSorted_Happy_Test() {
         val input = arrayListOf(6, 1, 3, 2, 5)
         bubbleSorted(input)
         assertEquals(1, input[0])
@@ -14,18 +14,19 @@ class SortedArrayTest {
         assertEquals(3, input[2])
         assertEquals(5, input[3])
         assertEquals(6, input[4])
+    }
 
-//        // ----------------------------------------------------------------
-//        val input1 = arrayListOf(1, 2, 8, -5, 6)
-//        assertEquals(6, bubbleSorted(input1))
-//
-//        // ----------------------------------------------------------------
-//        val input2 = arrayListOf(4, 4, 4, 4, 4)
-//        assertNull(bubbleSorted(input2))
-//
-//        // ----------------------------------------------------------------
-//        val input3 = arrayListOf(4, 4, 10, 4, 4)
-//        assertEquals(4, bubbleSorted(input3))
+    @Test
+    fun bubbleSorted_SameValueDoesNotMatter_Test() {
+        val input1 = arrayListOf(6, 1, 3, 2, 5, 4, 4)
+        bubbleSorted(input1)
+        assertEquals(1, input1[0])
+        assertEquals(2, input1[1])
+        assertEquals(3, input1[2])
+        assertEquals(4, input1[3])
+        assertEquals(4, input1[4])
+        assertEquals(5, input1[5])
+        assertEquals(6, input1[6])
     }
 
     private fun bubbleSorted(input: ArrayList<Int>) {
@@ -34,14 +35,16 @@ class SortedArrayTest {
         // iterate index 0 until max index - index i: j
         // if element j > next index -> swap
 
+        var n = 0
         for (i in input.indices) {
-            for (j in 0 until input.size - i) {
+            for (j in 0 until input.size - i - 1) {
                 val prev = input[j]
                 val next = input[j + 1]
                 if (prev > next) {
-                    input[i] = next
+                    input[j] = next
                     input[j + 1] = prev
                 }
+                println(++n)
             }
         }
     }
