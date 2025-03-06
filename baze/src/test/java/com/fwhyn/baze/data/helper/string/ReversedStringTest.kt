7b69d5b14c021fe35ade280f1e36b54f566a301f
fwhyn <fwhynseventh@gmail.com>
2025-03-06 09:19:02 +0700

    Test
    added reversed string test

--- baze/src/test/java/com/fwhyn/baze/data/helper/string/ReversedStringTest.kt
+++ baze/src/test/java/com/fwhyn/baze/data/helper/string/ReversedStringTest.kt
@@ -9,9 +9,31 @@ class ReversedStringTest {
     fun reverseStringTest() {
         val input = "abcde"
         Assert.assertEquals("edcba", reverseString(input))
+        Assert.assertEquals("edcba", reverseStringV2(input))
+
+        // ----------------------------------------------------------------
+        val input1 = "ab"
+        Assert.assertEquals("ba", reverseString(input1))
+        Assert.assertEquals("ba", reverseStringV2(input1))
+
+        // ----------------------------------------------------------------
+        val input2 = "abc"
+        Assert.assertEquals("cba", reverseString(input2))
+        Assert.assertEquals("cba", reverseStringV2(input2))
     }
 
     private fun reverseString(input: String): String {
+        val size = input.length
+        var output = ""
+
+        for (i in (size - 1) downTo 0) {
+            output += input[i]
+        }
+
+        return output
+    }
+
+    private fun reverseStringV2(input: String): String {
         val size = input.length
         val output = input.toCharArray()
 
