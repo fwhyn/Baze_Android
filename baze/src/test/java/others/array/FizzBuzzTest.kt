e838f11e9604d75389c261e7e77efa5b0e68a968
fwhyn <fwhynseventh@gmail.com>
2025-04-16 05:33:43 +0700

    Test
    FizzBuzz

---
+++ baze/src/test/java/others/array/FizzBuzzTest.kt
@@ -0,0 +1,26 @@
+import org.junit.Test
+
+class FizzBuzzTest {
+
+    @Test
+    fun fizzBuzzTest() {
+        for (i in 1..100) {
+            var output = ""
+
+            if (i % 3 == 0) {
+                output += "Fizz"
+            }
+
+            if (i % 5 == 0) {
+                output += "Buzz"
+            }
+
+            if (i % 7 == 0) {
+                output += "Bazz"
+            }
+
+            println(if (output == "") i else output)
+        }
+
+    }
+}
