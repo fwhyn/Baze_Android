f67d0c0b3d3e3e1d968e2a33ab9d0aa25c23e0eb
fwhyn <fwhynseventh@gmail.com>
2025-08-15 10:20:12 +0700

    Baze
    changed circular dialog implementations

--- baze/src/main/java/com/fwhyn/lib/baze/compose/dialog/CircularProgressDialog.kt
+++ baze/src/main/java/com/fwhyn/lib/baze/compose/dialog/CircularProgressDialog.kt
@@ -1,58 +1,57 @@
 package com.fwhyn.lib.baze.compose.dialog
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
 @Composable
 fun CircularProgressDialog(
-    progress: Int? = null,
+    foregroundModifier: Modifier = Modifier,
+    backgroundModifier: Modifier = Modifier,
+    color: Color = ProgressIndicatorDefaults.circularColor,
+    strokeWidth: Dp = ProgressIndicatorDefaults.CircularStrokeWidth,
+    trackColor: Color = ProgressIndicatorDefaults.circularIndeterminateTrackColor,
+    strokeCap: StrokeCap = ProgressIndicatorDefaults.CircularIndeterminateStrokeCap,
     onDismiss: () -> Unit = {},
+    trailingContent: (@Composable () -> Unit)? = null
 ) {
     Dialog(
         onDismissRequest = onDismiss,
     ) {
         Column(
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Box(
                 contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .size(80.dp)
-                    .background(Color.White, shape = CircleShape)
+                modifier = backgroundModifier
             ) {
                 CircularProgressIndicator(
-                    modifier = Modifier.size(60.dp),
-                    color = Color.Blue,
-                    strokeWidth = 6.dp
+                    modifier = foregroundModifier,
+                    color = color,
+                    strokeWidth = strokeWidth,
+                    trackColor = trackColor,
+                    strokeCap = strokeCap
                 )
             }
 
-            progress?.let {
-                Spacer(
-                    modifier = Modifier.height(5.dp)
-                )
-
-                Text(
-                    text = "$it %",
-                    textAlign = TextAlign.Center,
-                    color = Color.White,
-                )
-            }
+            trailingContent?.invoke()
         }
     }
 }
@@ -66,5 +65,18 @@ fun CircularProgressDialogPreview0() {
 @Preview
 @Composable
 fun CircularProgressDialogPreview1() {
-    CircularProgressDialog(70)
+    CircularProgressDialog(
+        foregroundModifier = Modifier
+            .padding(10.dp)
+            .background(color = Color.White, shape = CircleShape),
+        backgroundModifier = Modifier
+            .background(color = Color.White, shape = CircleShape),
+        strokeWidth = 8.dp
+    ) {
+        Text(
+            text = "$70 %",
+            textAlign = TextAlign.Center,
+            color = Color.White,
+        )
+    }
 }
