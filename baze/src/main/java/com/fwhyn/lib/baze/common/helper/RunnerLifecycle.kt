d4e113f877e161e7705329ff52d0665d99e724ce
fwhyn <fwhynseventh@gmail.com>
2025-08-22 13:47:16 +0700

    Refactor
    deleted unnecessary codes

--- baze/src/main/java/com/fwhyn/lib/baze/common/helper/RunnerLifecycle.kt
+++
@@ -1,8 +0,0 @@
-package com.fwhyn.lib.baze.common.helper
-
-interface RunnerLifecycle<PARAM, RESULT> {
-    fun onStart() {}
-    suspend fun onFetchParam(): PARAM
-    suspend fun onEmitResult(result: Result<RESULT>) {}
-    fun onFinish() {}
-}
