411fbc8aeb4e70659e7564eda6729e9199f98a67
fwhyn <fwhynseventh@gmail.com>
2026-02-25 11:40:41 +0700

    Calculator for Training

--- deandro/src/main/java/com/fwhyn/app/deandro/feature/presentation/login/CalculatorViewModel.kt
+++ deandro/src/main/java/com/fwhyn/app/deandro/feature/presentation/login/CalculatorViewModel.kt
@@ -2,15 +2,16 @@ package com.fwhyn.app.deandro.feature.presentation.login
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class CalculatorViewModel : ViewModel() {
 
     private val plus = Plus()
 
-    val result: MutableStateFlow<Double> = MutableStateFlow(0.0)
+    private val _result: MutableStateFlow<Double> = MutableStateFlow(0.0)
+    val result: StateFlow<Double> = _result
 
     fun onPlusClicked(a: Double, b: Double) {
-        val _result = plus(a, b)
-        result.value = _result
+        _result.value = plus(a, b)
     }
 }
